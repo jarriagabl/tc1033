93d03e4544add13f5cab22fb863b765509b3098e
jarriagabl <86971628+jarriagabl@users.noreply.github.com>
2025-11-21 13:10:05 -0600

    Added pass by ref vs pass by copy example

--- semana3/Point/prueba_point.cpp
+++ semana3/Point/prueba_point.cpp
@@ -4,13 +4,35 @@
 
 using std::cout;
 
+// Pasar por copia
+void cambia_copia(Point p)
+{
+    p.setX(15);
+    p.setY(25);
+}
+
+// Pasar por referencia
+void cambia_referencia(Point& p)
+{
+    p.setX(15);
+    p.setY(25);
+}
+
 int main()
 {
+    // Ejemplo uso de Point
     Point p1{5, 10};
     Point p2{};
 
-    p2.setX(10);
-    p1.setY(15);
+    cout << "Distancia entre (" << p1.getX() << ", " << p1.getY() << ") y (" << p2.getX() 
+        << ", " << p2.getY() << "): " << p1.distance(p2) << "\n";
 
-    cout << "La distancia entre los puntos es " << p1.distance(p2) << "\n";
+    // Ejemplo diferencia entre pasar por copia y pasar por referencia
+    cout << "Punto original: (" << p1.getX() << ", " << p1.getY() << ")\n";
+
+    cambia_copia(p1);
+    cout << "Punto después de cambia_copia: (" << p1.getX() << ", " << p1.getY() << ")\n";
+
+    cambia_referencia(p1);
+    cout << "Punto después de cambia_referencia: (" << p1.getX() << ", " << p1.getY() << ")\n";
 }
